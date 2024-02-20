51eb15776faf80f63ebac3416cb47d2e050494f1
Ferhat <parhat002@gmail.com>
2024-02-21 01:13:52 +0300

    02

---
+++ m2-generics/src/main/java/com/cydeo/Currency.java
@@ -0,0 +1,16 @@
+package com.cydeo;
+
+public enum Currency {
+
+    PENNY(1),NICKLE(5),DIME(10),QUARTER(25);
+
+    private int value;
+
+    Currency(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
