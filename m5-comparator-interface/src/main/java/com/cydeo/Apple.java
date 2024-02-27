fe9b52d8331ebd851f0080137ff928cf156b104b
Ferhat <parhat002@gmail.com>
2024-02-27 22:30:54 +0300

    comparator-interface

---
+++ m5-comparator-interface/src/main/java/com/cydeo/Apple.java
@@ -0,0 +1,14 @@
+package com.cydeo;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Apple {
+
+    private int weight;
+    private Color color;
+}
---
+++ m5-comparator-interface/src/main/java/com/cydeo/Main.java
@@ -0,0 +1,35 @@
+package com.cydeo;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        List<Integer> list = Arrays.asList(10,0,15,5,20);
+        System.out.println(list);
+
+        //Ascending
+        Collections.sort(list);
+        System.out.println(list);
+
+        //Descending
+        /*
+        Collections.reverse(list);
+         */
+        Collections.sort(list, new MyComparator());
+        System.out.println(list);
+
+        Collections.sort(list, ((o1, o2) -> (o1>o2) ? -1 : (o1<o2) ? 1 : 0));
+
+        //Ascending
+        list.sort(((o1, o2) -> o1.compareTo(o2)));
+        System.out.println(list);
+
+        //descending
+        list.sort(((o2, o1) -> o1.compareTo(o2)));
+        System.out.println(list);
+    }
+}
