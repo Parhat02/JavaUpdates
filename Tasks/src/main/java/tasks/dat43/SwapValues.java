b7414d714dbe29b5ddff5add8ca44b6f0e0503e4
Ferhat <parhat002@gmail.com>
2024-03-03 19:38:22 +0300

    Task

--- Tasks/src/main/java/tasks/dat43/SwapValues.java
+++ Tasks/src/main/java/tasks/dat43/SwapValues.java
@@ -6,8 +6,21 @@ public class SwapValues {
 
     public static void main(String[] args) {
 
-        Integer[] a = {1, 2, 3, 4};
+        Integer[] a = {4,2,3,8,9,15,12,1,1,2,25,65};
         swap(a, 0, 2);
+        System.out.println(Arrays.toString(a));
+
+        for (int i = 0; i < a.length-1; i++) {
+            for (int j = i+1; j < a.length; j++) {
+                int temp = a[j];
+                if (a[i] > a[j]){
+                    a[j] = a[i];
+                    a[i] = temp;
+                }
+
+            }
+
+        }
         System.out.println(Arrays.toString(a));
     }
     public static <T> void swap(T[] a, int b, int c){
