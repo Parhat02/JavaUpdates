6dcde9a443ec8f6ce508b0b91b6b13e6f4ea981e
Ferhat <parhat002@gmail.com>
2024-03-04 23:02:20 +0300

    modularity-private

--- m8-new-version-updates/src/main/java/com/cydeo/java9/CompactStringDemo.java
+++ m8-new-version-updates/src/main/java/com/cydeo/java9/CompactStringDemo.java
@@ -1,9 +1,15 @@
 package com.cydeo.java9;
 
+import java.time.LocalDate;
+
 public class CompactStringDemo {
 
     public static void main(String[] args) {
 
         String abc = "cydeo";
+
+        Test test = new Test();
+        LocalDate date1 = LocalDate.of(2003,1,1);
+        System.out.println(test.nextDay(date1));
     }
 }
---
+++ m8-new-version-updates/src/main/java/com/cydeo/java9/Test.java
@@ -0,0 +1,22 @@
+package com.cydeo.java9;
+
+import java.time.LocalDate;
+
+public class Test implements PrivateMethodInterface{
+
+
+    @Override
+    public boolean isHoliday(LocalDate date) {
+        return false;
+    }
+
+    @Override
+    public boolean isBusinessDay(LocalDate date) {
+        return PrivateMethodInterface.super.isBusinessDay(date);
+    }
+
+    @Override
+    public LocalDate nextDay(LocalDate date) {
+        return PrivateMethodInterface.super.nextDay(date);
+    }
+}
