fdb1ade3ba6bc0760b6bec2ce8d30198a285d0f5
Ferhat <parhat002@gmail.com>
2024-03-05 23:23:28 +0300

    Updates_After_Java11

---
+++ m8-new-version-updates/src/main/java/com/cydeo/java11/StringMethodDemo.java
@@ -0,0 +1,11 @@
+package com.cydeo.java11;
+
+public class StringMethodDemo {
+
+    public static void main(String[] args) {
+
+        var str = "Mike \n Ozzy \n Tom \n Emmy";
+
+        str.lines().forEach(System.out::println);  //line() : it is used to get all the lines in a String as a Stream
+    }
+}
