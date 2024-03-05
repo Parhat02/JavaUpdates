b9630de5cfe60f1cbbe2326a1c0dfdb0ddb17b1f
Ferhat <parhat002@gmail.com>
2024-03-05 23:55:18 +0300

    Updates_After_Java14

---
+++ m8-new-version-updates/src/main/java/com/cydeo/java14/User.java
@@ -0,0 +1,13 @@
+package com.cydeo.java14;
+
+public record User(String name, String address) { //Each field is private final
+
+    //ALL ARG CONST
+    //GETTER
+    //TO STRING
+    //EQUALS
+    //HASH CODE
+
+
+    //BREAK TILL 3:30
+}
